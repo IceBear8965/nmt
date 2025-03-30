db64ab96ff72bf8df06cf8e87a2a957254bee4bd
IceBear8965 <icebee088@gmail.com>
2025-03-30 15:29:36 +0300

    Fixed sidebar links on test pages

--- scripts/menu-toggle.js
+++ scripts/menu-toggle.js
@@ -1,23 +1,25 @@
-$(document).ready(function(){
-	$('#nav-toggle').click(function(){
-		$(this).toggleClass('open');
-		$(".sidebar").toggleClass('open');
-		$("body").toggleClass('body--fixed');
-	});
+$(document).ready(function () {
+  $("#nav-toggle").click(function () {
+    $(this).toggleClass("open");
+    $(".sidebar").toggleClass("open");
+    $("body").toggleClass("body--fixed");
+  });
 });
 
-$(".sidebar_link").on("click", function(event){
+$(".sidebar_link").on("click", function (event) {
   event.preventDefault();
-  $("#nav-toggle").toggleClass('open');
-  $(".sidebar").toggleClass('open');
-  $("body").toggleClass('body--fixed');
+  $("#nav-toggle").toggleClass("open");
+  $(".sidebar").toggleClass("open");
+  $("body").toggleClass("body--fixed");
   let $this = $(this);
-  let blockId = $this.attr('href');
+  let blockId = $this.attr("href");
   let headerHeight = $(".header").outerHeight();
   let blockOffset = $(blockId).offset().top - headerHeight;
 
-  $("html, body").animate({
-    scrollTop: blockOffset
-  }, 500);
-})
-
+  $("html, body").animate(
+    {
+      scrollTop: blockOffset,
+    },
+    500,
+  );
+});
